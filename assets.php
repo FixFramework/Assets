9e664a8df7a5c758a237fd23fa184bb80064d6b3
Cengiz AKCAN <cengiz@zekicetasarim.com>
2018-07-12 02:58:01 +0300

    Add files via upload

---
+++ assets.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace assets;
+
+use Fix\Support\Support;
+use Fix\Kernel\Kernel;
+use Fix\Kernel\Url;
+use Fix\Settings\Kernel as Settings;
+
+class Pages
+{
+
+    /**
+     * @param null $__setFile
+     * @param null $__setData
+     * @return bool
+     */
+    public static function render($__setFile = null, $__setData = null){
+
+        if($__setFile):
+
+            if(
+                file_exists
+                (
+            Settings::APPLICATIONS_MASTER_FOLDER.
+                    Settings::SLASH.
+                    Url::getSettings()["folder"].
+                    Settings::SLASH.
+                    Settings::VIEWS_FOLDER.
+                    Settings::SLASH.
+                    Settings::ASSETS_PAGES_FOLDER.
+                    Settings::SLASH.
+                    $__setFile.
+                    Settings::CORE_EXTENSION
+                )
+            ):
+
+                is_array($__setData) ? extract($__setData) : null;
+
+                include
+                (
+                    Settings::APPLICATIONS_MASTER_FOLDER.
+                    Settings::SLASH.
+                    Url::getSettings()["folder"].
+                    Settings::SLASH.
+                    Settings::VIEWS_FOLDER.
+                    Settings::SLASH.
+                    Settings::ASSETS_PAGES_FOLDER.
+                    Settings::SLASH.
+                    $__setFile.
+                    Settings::CORE_EXTENSION
+                );
+
+                return true;
+
+            else:
+                Support::__error("NOT FOUND FILE");
+            endif;
+
+        else:
+            Support::__error("ENTER FILE NAME");
+        endif;
+
+    }
+
+}
